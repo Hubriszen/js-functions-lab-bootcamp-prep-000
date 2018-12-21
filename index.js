b7854ae4d0fea1af9e8fed56d1cb21c712ff4298
M H <ptcb795@gmail.com>
2018-12-21 17:41:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 function happyHolidays(){return 'Happy Holidays!'}
 function happyHolidayTo(holiday, name) {return `Happy ${holiday}, ${name}!`}
 function holidayCountdown(days, holiday) {return `It's ${days} days until ${holiday}!`}
-function happyHolidays() return {Happy Holidays!}
-function happyHolidaysTo() return `Happy $holiday, $name!'
+function happyHolidays() return {
+  Happy Holidays!
+  
+}
+
+
+function happyHolidaysTo() {
+return `Happy $holiday, $name!'
 function 
 >>>>>>> b669409bea0c2be3bee801a675879c1d731f1a03
 
