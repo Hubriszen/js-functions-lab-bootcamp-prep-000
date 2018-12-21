256b3ea95adfbe7b067a11eb8017e90d13c8c2f8
M H <ptcb795@gmail.com>
2018-12-21 17:41:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function happyHolidays() return {Happy Holidays!}
 function happyHolidaysTo() return `Happy $holiday, $name!'
 function 
 >>>>>>> b669409bea0c2be3bee801a675879c1d731f1a03
+
+
+function nameOfFunction() {
+  
+  
+  
+}
