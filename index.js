f0adf2eeca5a72319b037ea9363289a093548a57
M H <ptcb795@gmail.com>
2018-12-21 17:52:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}!`
 }
 
-function happyHolidays() {
-  return "Happy Holidays!"
-}
-
 function happyHolidaysTo() {
   return 'Happy !'
 }
